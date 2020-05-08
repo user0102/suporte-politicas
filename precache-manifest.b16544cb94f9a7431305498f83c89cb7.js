379ddb44c14bb01b196362294211aeba46095796
tomas <tomiulloa2002@gmail.com>
2020-05-08 14:07:23 -0400

    Updates

---
+++ precache-manifest.b16544cb94f9a7431305498f83c89cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3dcf4800ed0a9d6732e6468db89c1b",
+    "url": "/suporte-politicas/index.html"
+  },
+  {
+    "revision": "367b354cb762d8096e68",
+    "url": "/suporte-politicas/static/css/main.ee58f6ed.chunk.css"
+  },
+  {
+    "revision": "f5e528ad4c75a9fcdb5c",
+    "url": "/suporte-politicas/static/js/2.7949c839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suporte-politicas/static/js/2.7949c839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367b354cb762d8096e68",
+    "url": "/suporte-politicas/static/js/main.f2955111.chunk.js"
+  },
+  {
+    "revision": "b20db60084a5c2fe75e8",
+    "url": "/suporte-politicas/static/js/runtime-main.20ceb8f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suporte-politicas/static/media/logo.5d5d9eef.svg"
+  }
+]);
